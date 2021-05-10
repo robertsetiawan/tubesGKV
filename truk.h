b5ef8d2004e3ffd87ca6998c781e28ab836e8328
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-05-10 15:29:32 +0700

    pisah objek2 truk
    prosedur container & wheelDepan

--- truk.h
+++ truk.h
@@ -2,6 +2,8 @@
 #define TRUK_H_INCLUDED
 
 void wheel(float x, float y, float z);
+void container(int posisi, float x, float y, float z);
+void wheelDepan(float x, float y, float z);
 void Truk(float x, float y, float z);
 
 #endif // TRUK_H_INCLUDED
