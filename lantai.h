65c5e5cac9ca35f9be7a9fa4ec59a89391d0f3b6
concernedmate <66343506+concernedmate@users.noreply.github.com>
2021-05-17 10:39:37 +0700

    kurang semicolon

--- lantai.h
+++ lantai.h
@@ -2,9 +2,5 @@
 #define LANTAI_H_INCLUDED
 
 void Jalan();
-void lantai();
-void trotoar(float x);
-void roadsideRight(float x);
-void roadsideLeft(float x)
-#endif // LANTAI_H_INCLUDED
 
+#endif // LANTAI_H_INCLUDED
