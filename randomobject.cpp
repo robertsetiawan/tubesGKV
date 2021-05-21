a452126be982abbae3b79cc0953ca09218357513
concernedmate <66343506+concernedmate@users.noreply.github.com>
2021-05-22 02:27:34 +0700

    Update collision, and gameplay mechanics
    -add some buildings
    -add coins(pickable) and scores (no gui yet)
    -fix collision
    -fix setir

--- randomobject.cpp
+++ randomobject.cpp
@@ -21,6 +21,10 @@ void Objek::setSize(float s)
     bsize = s;
 }
 
+void Objek::setHitValue(int v){
+    bhitval = v;
+}
+
 float Objek::getX()
 {
     return bx;
@@ -41,6 +45,10 @@ float Objek::getSize()
     return bsize;
 }
 
+int Objek::getHitValue()
+{
+    return bhitval;
+}
 
 Batu::Batu(float x, float y, float z, float s)
 {
@@ -48,13 +56,29 @@ Batu::Batu(float x, float y, float z, float s)
     setY(y);
     setZ(z);
     setSize(s);
+    setHitValue(0);
 
     glPushMatrix();
     glTranslatef(getX(),getY(), getZ());
         glPushMatrix();
-            glColor3f(1.0,0.0,0.0);
+            glColor3f(0.5,0.25,0.5);
             glutSolidCube(getSize());
         glPopMatrix();
     glPopMatrix();
 }
 
+Coin::Coin(float x, float y, float z, float s){
+    setX(x);
+    setY(y);
+    setZ(z);
+    setSize(s);
+    setHitValue(10);
+
+    glPushMatrix();
+    glTranslatef(getX(),getY(), getZ());
+        glPushMatrix();
+            glColor3f(0.85,0.85,0.15); //warna kuning :)
+            glutSolidTorus(getSize()/2,getSize(),5,5);
+        glPopMatrix();
+    glPopMatrix();
+}
